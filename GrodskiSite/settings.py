321252c10103838a20d7422f8771bb903fff1c78
quintindunn <93884113+quintindunn@users.noreply.github.com>
2022-04-08 02:21:11 -0400

    Update settings.py

--- GrodskiSite/settings.py
+++ GrodskiSite/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+# Don't worry, this will be changed for production <3
 SECRET_KEY = 'django-insecure-_zv9ax+fqrci19&u)(7$*ugv9+*v0lm5@3iptkguch^7qjsds&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
